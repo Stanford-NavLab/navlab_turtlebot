ba9661e6dd2c7dda838e424e4b6c4cbf8d94ee09
isabellastorres <izzietorres@yahoo.com>
2023-04-10 11:42:02 -0700

    fixed topic issue

--- navlab_turtlebot_comms/src/comms.py
+++ navlab_turtlebot_comms/src/comms.py
@@ -21,16 +21,18 @@ def robot2_cb(data):
     trajs[1] = data
 
 def zonotope_cb(data):
+    print("here's some zonotopes")
     zonotopes = data
 
 def check_occlusions():
-    print(trajs[1])
     if len(trajs[1].states)>0:
         print(trajs[1].states[0].x)
     if len(trajs[0].states)>0 and len(trajs[1].states)>0:
         robot2_pos = trajs[1].states[0].x
         print(robot2_pos)
-        #for obs in obstacles:
+        for obs in zonotopes:
+            print(obs)
+            print(" ")
             #if not check_obs_collisions(positions,obs,2*params.R_BOT):
                 #return False
     return True
@@ -44,9 +46,9 @@ def comms():
     while not rospy.is_shutdown():
         rospy.Subscriber('/turtlebot1/planner/traj',NominalTrajectory,robot1_cb)
         rospy.Subscriber('/turtlebot2/planner/traj',NominalTrajectory,robot2_cb)
-        rospy.Subscriber('/map/Zonotopes',ZonotopeMsgArray,zonotope_cb)
+        rospy.Subscriber('/map/zonotopes',ZonotopeMsgArray,zonotope_cb)
         # Probability 1% of comms dropping each .1 sec, comms drop for occlusions
-        if True: #check_occlusions():
+        if check_occlusions():
             no_comms = np.random.choice([0,1],size=(1,2),p=[.99,.01])
         else:
             no_comms = np.array([1,1])
