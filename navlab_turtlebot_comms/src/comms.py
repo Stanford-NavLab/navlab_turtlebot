97c7d7987b3e8a33800fa5c654a92b88bcac76cf
isabellastorres <izzietorres@yahoo.com>
2023-05-18 16:53:09 -0700

    Updated occlusions modelling to work for up to four turtlebots

--- navlab_turtlebot_comms/src/comms.py
+++ navlab_turtlebot_comms/src/comms.py
@@ -9,11 +9,12 @@ from navlab_turtlebot_common.msg import State
 from navlab_turtlebot_obstacles.msg import ZonotopeMsg, ZonotopeMsgArray
 from zonotope import Zonotope
 from gazebo_msgs.msg import ModelStates
+import argparse
 
 class comms_node():
-    def __init__(self):
+    def __init__(self,n):
         # Dubins planner
-        self.n = 2
+        self.n = int(n)
         self.trajs = [NominalTrajectory()]*self.n
         self.zonotopes = []
         self.down = False
@@ -25,6 +26,12 @@ class comms_node():
     def robot2_cb(self,data):
         self.trajs[1] = data
 
+    def robot3_cb(self,data):
+        self.trajs[2] = data
+
+    def robot4_cb(self,data):
+        self.trajs[3] = data
+
     def gazebo_cb(self,data):
         robot1 = data.name.index("turtlebot1")
         robot2 = data.name.index("turtlebot2")
@@ -32,32 +39,29 @@ class comms_node():
         self.locs[0,1] = data.pose[robot1].position.y
         self.locs[1,0] = data.pose[robot2].position.x
         self.locs[1,1] = data.pose[robot2].position.y
+        if self.n >= 3:
+            robot3 = data.name.index("turtlebot3")
+            self.locs[2,0] = data.pose[robot3].position.x
+            self.locs[2,1] = data.pose[robot3].position.y
+            if self.n >= 4:
+                robot4 = data.name.index("turtlebot4")
+                self.locs[3,0] = data.pose[robot4].position.x
+                self.locs[3,1] = data.pose[robot4].position.y
 
     def zonotope_cb(self,data):
         self.zonotopes = data.zonotopes
         #print(self.zonotopes[0].generators)
 
-    def check_occlusions(self):
-        #if len(trajs[1].states)>0:
-            #print(trajs[1].states[0].x)
-        #if len(self.trajs[0].states)>0 and len(self.trajs[1].states)>0:
-            #r1_x = self.trajs[0].states[0].x
-            #r1_y = self.trajs[0].states[0].y
-            #r2_x = self.trajs[1].states[0].x
-            #r2_y = self.trajs[1].states[0].y
-            #print(r2_x,r2_y)
-            #r_m = (r2_y-r1_y)/(r2_x-r1_x)
-            #r_b = r_m*r2_x+r2_y
-        r_m = (self.locs[1,1]-self.locs[0,1])/(self.locs[1,0]-self.locs[0,0])
-        r_b = r_m*self.locs[1,0] + self.locs[1,1]
+    def check_occlusions(self,zero,one):
+        """
+        Checks if the path between robot zero and robot one is obstructed by a 2D obstacle.
+        """
+        r_m = (self.locs[one,1]-self.locs[zero,1])/(self.locs[one,0]-self.locs[zero,0])
+        r_b = r_m*self.locs[one,0] + self.locs[one,1]
         for obs in self.zonotopes:
-            #print(np.array(obs.generators).reshape((obs.dim,obs.num_gen)))
             c = np.array(obs.generators).reshape((obs.dim,obs.num_gen))[:,0].reshape((obs.dim,1))
-            #print("c",c[:2])
             g = np.array(obs.generators).reshape((obs.dim,obs.num_gen))[:,1:]
-            #print("g",g[:2,:-1])
             vertices = Zonotope(c[:2],g[:2,:-1]).vertices()
-            print("v",vertices)
             for i in range(vertices.shape[1]-1):
                 p1 = vertices[:2,i]
                 p2 = vertices[:2,i+1]
@@ -69,7 +73,7 @@ class comms_node():
                         x = p2[0]
                         y = r_m*x+r_b
                     elif np.absolute(r_m) == np.inf:
-                        x = self.locs[0,0]
+                        x = self.locs[zero,0]
                         y = m*x+b
                     else:
                         x = (r_b-b)/(m-r_m)
@@ -77,23 +81,23 @@ class comms_node():
                     # If the point is on the line segments, the difference between x and bounds should have opposite signs to be in between
                     check1 = x-p1[0]
                     check2 = x-p2[0]
-                    check3 = x-self.locs[0,0]
-                    check4 = x-self.locs[1,0]
+                    check3 = x-self.locs[zero,0]
+                    check4 = x-self.locs[one,0]
                     if (abs(check1)/(check1)!=abs(check2)/(check2) and \
                        abs(check3)/(check3)!=abs(check4)/(check4)) or \
                        (np.absolute(m+r_m)==np.inf and \
                        abs(y-p1[1])/(y-p1[1])!=abs(y-p2[1])/(y-p2[1]) and \
-                       abs(y-self.locs[0,1])/(y-self.locs[0,1])!=abs(y-self.locs[1,1])/(y-self.locs[1,1])):
+                       abs(y-self.locs[zero,1])/(y-self.locs[zero,1])!=abs(y-self.locs[one,1])/(y-self.locs[one,1])):
                         #print(x,y)
                         #print(self.locs)
                         return False
                 else: # Assumes the lines are roughly parallel
                     # Is the first point on the line between robots?
-                    check1 = p1[0]-self.locs[0,0]
-                    check2 = p1[0]-self.locs[1,0]
+                    check1 = p1[0]-self.locs[zero,0]
+                    check2 = p1[0]-self.locs[one,0]
                     # Is the second point on the line between robots?
-                    check3 = p2[0]-self.locs[0,0]
-                    check4 = p2[0]-self.locs[1,0]
+                    check3 = p2[0]-self.locs[zero,0]
+                    check4 = p2[0]-self.locs[one,0]
                     if abs(check1)/check1!=abs(check2)/check2 and \
                        abs(check3)/check3!=abs(check4)/check4 and \
                        abs(b-r_b)<.001: # Only return false if collinear
@@ -102,24 +106,48 @@ class comms_node():
 
     def comms(self):
         pub = rospy.Publisher('comms_list',Int16MultiArray,queue_size=10)
-        #pub1 = rospy.Publisher('/turtlebot1/comms',NominalTrajectory,queue_size=10)
-        #pub2 = rospy.Publisher('/turtlebot2/comms',NominalTrajectory,queue_size=10)
         rospy.init_node('comms',anonymous=True)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            #rospy.Subscriber('/turtlebot1/planner/traj',NominalTrajectory,self.robot1_cb)
-            #rospy.Subscriber('/turtlebot2/planner/traj',NominalTrajectory,self.robot2_cb)
             rospy.Subscriber('/gazebo/model_states',ModelStates,self.gazebo_cb)
             rospy.Subscriber('/map/zonotopes',ZonotopeMsgArray,self.zonotope_cb)
             # Probability 1% of comms dropping each .1 sec, comms drop for occlusions
-            if self.check_occlusions():
-                no_comms = np.random.choice([0,1],size=(1,2),p=[.99,.01])
-            else:
-                no_comms = np.array([1,1]).reshape((1,2))
+            randloss = np.random.choice([0,1],size=(self.n,self.n),p=[.99,.01])
+            comms_group = np.array([np.inf]*self.n)
+            group = 0
+            comms_group[0] = 0
+            old_group = np.copy(comms_group)
+            # Until each robot has been assigned a group
+            while np.sum(comms_group) == np.inf:
+                # for each robot
+                for i in range(self.n):
+                    # if it is in the current group we are assigning robots to
+                    if comms_group[i] == group:
+                        # for each robot
+                        for j in range(self.n):
+                            # if it isn't the same robot and it doesn't already have a group
+                            if not j==i and comms_group[j]==np.inf:
+                                # If the line of sight is not blocked:
+                                if self.check_occlusions(i,j) and randloss[i,j]==0:
+                                    # Assign to the same comms group
+                                    comms_group[j] = comms_group[i]
+                # If the last iteration didn't change anything,
+                if np.array_equal(comms_group, old_group):
+                    # Add a new comms group
+                    group += 1
+                    # If we are at one robot left, just stop
+                    if np.sum(np.where(comms_group==np.inf,1,0))==1:
+                        comms_group = np.where(comms_group==np.inf,group,comms_group)
+                old_group = np.copy(comms_group)
+            #if self.check_occlusions():
+                #no_comms = np.random.choice([0,1],size=(1,2),p=[.99,.01])
+            #else:
+                #no_comms = np.array([1,1]).reshape((1,2))
             # If comms are working, robot1 gets the info from robot2, and vice versa
-            comms_list = Int16MultiArray(data=list(no_comms[0]))
+            #comms_list = Int16MultiArray(data=list(no_comms[0]))
+            comms_list = Int16MultiArray(data=list(comms_group))
             pub.publish(comms_list)
-            if np.sum(no_comms)==0:
+            if np.sum(comms_group)==0:
                 #pub1.publish(trajs[1])
                 #pub2.publish(trajs[0])
                 if self.down:
@@ -132,5 +160,8 @@ class comms_node():
             rate.sleep()
 
 if __name__ == '__main__':
-    node = comms_node()
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-num", "--number", help="number of robots")
+    args = argParser.parse_args()
+    node = comms_node(args.number)
     node.comms()
