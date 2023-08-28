38a365a51892264faa6643acb28c6dcb3e34dda5
grandmasterizzie <izzietorres@yahoo.com>
2023-08-27 23:35:59 -0700

    Update frs_generator.py
    I'm not subscribing correctly

--- navlab_turtlebot_frs/src/frs_generator.py
+++ navlab_turtlebot_frs/src/frs_generator.py
@@ -124,7 +124,8 @@ class frs_generator:
                 # Subscribe to the global plans for every other agent
                 for i in range(self.n_bots):
                     if i!=int(self.name[-1]):
-                        rospy.Subscriber("/turtlebot" + str(i) + "2/move_base/TebLocalPlannerROS/global_plan", Path, self.traj_cb, (i))
+                        print("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan")
+                        rospy.Subscriber("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan", Path, self.traj_cb, (i))
             self.publish_frs()
             self.rate.sleep()
 
