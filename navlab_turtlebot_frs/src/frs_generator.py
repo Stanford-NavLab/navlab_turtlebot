6cb583d6d5343160cd15885faf26bdcdb132197a
grandmasterizzie <izzietorres@yahoo.com>
2023-08-29 02:26:51 -0700

    Update frs_generator.py
    only take first global plan

--- navlab_turtlebot_frs/src/frs_generator.py
+++ navlab_turtlebot_frs/src/frs_generator.py
@@ -145,11 +145,12 @@ class frs_generator:
             self.t_sim = rospy.get_time() - self.start
             # If we haven't received the trajectories yet
             if np.sum(self.received)!=self.n_bots:
-                # Subscribe to the global plans for every other agent
-                for i in range(self.n_bots):
-                    if i!=int(self.name[-1]):
-                        print("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan")
-                        rospy.Subscriber("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan", Path, self.traj_cb, (i))
+            # Subscribe to the global plans for every other agent
+            for i in range(self.n_bots):
+                # If we haven't received the trajectory yet
+                if self.received[i]==0 and i!=int(self.name[-1]):
+                    print("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan")
+                    rospy.Subscriber("/turtlebot" + str(i+1) + "/move_base/TebLocalPlannerROS/global_plan", Path, self.traj_cb, (i))
             self.publish_frs()
             self.rate.sleep()
 
