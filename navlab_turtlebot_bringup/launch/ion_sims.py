a74ffba71979b0350736f380bc1126e3292639a7
grandmasterizzie <izzietorres@yahoo.com>
2023-08-30 07:19:51 -0700

    Update ion_sims.py
    more debugging

--- navlab_turtlebot_bringup/launch/ion_sims.py
+++ navlab_turtlebot_bringup/launch/ion_sims.py
@@ -62,10 +62,12 @@ for sim in range(n_sims):
     
     print("ending")
         
-    print("Before closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(args)+'.csv', delimiter=',').shape)    
+    print("Before closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(0)+'.csv', delimiter=',').shape) 
+    print("Before closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(1)+'.csv', delimiter=',').shape) 
         
     # Close everything
     launch_ion.shutdown()
     launch_nm.shutdown()
     
-    print("After closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(args)+'.csv', delimiter=',').shape)
+    print("After closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(0)+'.csv', delimiter=',').shape)
+    print("After closing", np.loadtxt('/home/izzie/catkin_ws/src/navlab_turtlebot/navlab_turtlebot_frs/data/calodom'+str(1)+'.csv', delimiter=',').shape)
